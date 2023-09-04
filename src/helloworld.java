6dc97d6813b483677dac2f8d00515a55ad63d097
CheGlenn <118393138+CheGlenn@users.noreply.github.com>
2023-09-04 11:09:20 -0500

    Update helloworld.java

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,7 +2,7 @@ import java.io.PrintWriter;
 public class helloworld {
   public static void main (String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    pen.println ("Hello, GitHub!");
+    pen.println ("Hello!");
     pen.flush();
   } // main(String[])
 }
