e3c7b913fea98b46e5d774bf55ea37506b126754
CheGlenn <glennche@grinnell.edu>
2023-09-04 11:08:24 -0500

    Hello world

---
+++ src/helloworld.java
@@ -0,0 +1,8 @@
+import java.io.PrintWriter;
+public class helloworld {
+  public static void main (String[] args) {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    pen.println ("Hello, GitHub!");
+    pen.flush();
+  } // main(String[])
+}
